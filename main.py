9f967937b0ab1519b2b47fb9e0f8597f52166ce3
Mikael Gabryel Flor de Lis <gabryel.flordelis@gmail.com>
2024-04-09 23:56:59 +0400

    Added minicourt background

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from utils import (read_video,
 
 from trackers import PlayerTracker, BallTracker
 from court_line_detector import CourtLineDetector
+from mini_court import MiniCourt
 
 import cv2
 
@@ -15,11 +16,11 @@ def main():
     player_tracker = PlayerTracker(model_path='yolov8x')
     ball_tracker = BallTracker(model_path='models/yolo5_last.pt')
     player_detections = player_tracker.detect_frames(video_frames, 
-                                                     read_from_stub=False, 
+                                                     read_from_stub=True, 
                                                      stub_path='tracker_stubs/player_detections.pkl')
 
     ball_detections = ball_tracker.detect_frames(video_frames, 
-                                                     read_from_stub=False, 
+                                                     read_from_stub=True, 
                                                      stub_path='tracker_stubs/ball_detections.pkl')
 
     ball_detections = ball_tracker.interpolate_ball_positions(ball_detections)
@@ -32,6 +33,8 @@ def main():
     # Choose players
     player_detections = player_tracker.choose_and_filter_players(player_detections, court_kps)
 
+    # Minicourt
+    mini_court = MiniCourt(video_frames[0])
 
     # Draw output
 
@@ -43,6 +46,9 @@ def main():
     ## Draw court keypoints
     output_video_frames = court_line_detector.draw_kps_on_video(output_video_frames, court_kps)
     
+    ## Draw mini court
+    output_video_frames = mini_court.draw_mini_court(output_video_frames)
+
     ## Draw frame number on top left corner
     for i, frame in enumerate(output_video_frames):
         cv2.putText(frame, f"Frame: {i}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
--- mini_court/mini_court.py
+++ mini_court/mini_court.py
@@ -1,5 +1,6 @@
 import cv2
 import sys
+import numpy as np
 sys.path.append('../')
 import constants
 from utils import convert_meters_to_pixel_distance, convert_pixel_distance_to_meters
@@ -13,9 +14,12 @@ class MiniCourt():
         
         self.set_canvas_background_box_position(frame)
         self.set_mini_court_position()
+        self.set_court_drawing_kps()
+        self.set_court_lines()
 
     def convert_meters_to_pixels(self, meters):
         return convert_meters_to_pixel_distance(meters, constants.DOUBLE_LINE_WIDTH, self.court_drawing_width)
+
     def set_court_drawing_kps(self):
         drawing_kps = [0]*28
 
@@ -29,6 +33,54 @@ class MiniCourt():
         # point 3
         drawing_kps[6] = drawing_kps[0] + self.court_drawing_width
         drawing_kps[7] = drawing_kps[5]
+        # #point 4
+        drawing_kps[8] = drawing_kps[0] +  self.convert_meters_to_pixels(constants.DOUBLE_ALLEY_DIFFERENCE)
+        drawing_kps[9] = drawing_kps[1] 
+        # #point 5
+        drawing_kps[10] = drawing_kps[4] + self.convert_meters_to_pixels(constants.DOUBLE_ALLEY_DIFFERENCE)
+        drawing_kps[11] = drawing_kps[5] 
+        # #point 6
+        drawing_kps[12] = drawing_kps[2] - self.convert_meters_to_pixels(constants.DOUBLE_ALLEY_DIFFERENCE)
+        drawing_kps[13] = drawing_kps[3] 
+        # #point 7
+        drawing_kps[14] = drawing_kps[6] - self.convert_meters_to_pixels(constants.DOUBLE_ALLEY_DIFFERENCE)
+        drawing_kps[15] = drawing_kps[7] 
+        # #point 8
+        drawing_kps[16] = drawing_kps[8] 
+        drawing_kps[17] = drawing_kps[9] + self.convert_meters_to_pixels(constants.NO_MANS_LAND_HEIGHT)
+        # # #point 9
+        drawing_kps[18] = drawing_kps[16] + self.convert_meters_to_pixels(constants.SINGLES_LINE_WIDTH)
+        drawing_kps[19] = drawing_kps[17] 
+        # #point 10
+        drawing_kps[20] = drawing_kps[10] 
+        drawing_kps[21] = drawing_kps[11] - self.convert_meters_to_pixels(constants.NO_MANS_LAND_HEIGHT)
+        # # #point 11
+        drawing_kps[22] = drawing_kps[20] +  self.convert_meters_to_pixels(constants.SINGLES_LINE_WIDTH)
+        drawing_kps[23] = drawing_kps[21] 
+        # # #point 12
+        drawing_kps[24] = int((drawing_kps[16] + drawing_kps[18])/2)
+        drawing_kps[25] = drawing_kps[17] 
+        # # #point 13
+        drawing_kps[26] = int((drawing_kps[20] + drawing_kps[22])/2)
+        drawing_kps[27] = drawing_kps[21]
+        
+        self.drawing_kps = drawing_kps
+
+    def set_court_lines(self):
+        self.lines = [
+             (0, 2),
+            (4, 5),
+            (6,7),
+            (1,3),
+            
+            (0,1),
+            (8,9),
+            (10,11),
+            (10,11),
+            (2,3)
+
+        ]
+
 
 
     def set_mini_court_position(self):
@@ -46,3 +98,20 @@ class MiniCourt():
         self.end_y = self.buffer + self.drawing_rectangle_height
         self.start_x = self.end_x - self.drawing_rectangle_width
         self.start_y = self.end_y - self.drawing_rectangle_height
+
+    def draw_background_rectangle(self, frame):
+        shapes = np.zeros_like(frame, np.uint8)
+        # Draw the rectangle
+        cv2.rectangle(shapes, (self.start_x, self.start_y), (self.end_x, self.end_y), (255, 255, 255), cv2.FILLED)
+        out = frame.copy()
+        alpha = 0.5
+        mask = shapes.astype(bool)
+        out[mask] = cv2.addWeighted(frame, alpha, shapes, 1 - alpha, 0 )[mask]
+        return out
+    
+    def draw_mini_court(self, frames):
+        output_frames = []
+        for frame in frames:
+            frame = self.draw_background_rectangle(frame)
+            output_frames.append(frame)
+        return output_frames
